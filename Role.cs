f7eeb945c71cb470339ac8e2f9513324e2ad9b29
Ashlen-FPT <107978815+Ashlen-FPT@users.noreply.github.com>
2022-10-18 08:04:31 +0200

    Created System Roles

---
+++ Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralCargoSystem.Utility
+{
+    public static class Role
+    {
+        public const string Administrator = "Administrator";
+        public const string Supervisor= "Supervisor";
+        public const string User= "User";
+        
+        
+    }
+}
